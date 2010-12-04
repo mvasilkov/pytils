e9c6037b71dc608ec4382cab9c7207ce24bed891
Mark Vasilkov <mvasilkov@gmail.com>
2010-12-04 15:38:18 +0200

    `ru_date` filter format string doesn’t need percents anymore, like Django’s `date` (i.e. "Y-m-d", not "%Y-%m-%d")

--- pytils/templatetags/pytils.py
+++ pytils/templatetags/pytils.py
@@ -6,8 +6,15 @@ Some of the most useful filters, named to better match Django’s built-ins.
 from django import template
 from pytils_dt import ru_strftime as ru_date
 from pytils_numeral import get_plural as ru_plural
+import re
 
 register = template.Library()
 
-register.filter('ru_date', ru_date)
+def _percent_fmt(f):
+    def _f(n, s):
+        s = re.sub(r'(?=a|A|b|B|c|d|f|H|I|j|m|M|p|S|U|w|W|x|X|y|Y|z|Z|%)', '%', s)
+        return f(n, s)
+    return _f
+
+register.filter('ru_date', _percent_fmt(ru_date))
 register.filter('ru_plural', ru_plural)
